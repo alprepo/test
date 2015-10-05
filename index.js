b2b18f8db0f99e07a99539cff3c2a657291ba3da
Alex <alclever7@gmail.com>
2015-10-06 01:09:34 +0300

    Rejected commit: Default

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 //Ваш код будет здесь
+window.addEventListener('load',function(){
+    var button = document.querySelector(".startNewGame");
+    var field =document.querySelector(".field");
+    var cells = document.querySelectorAll(".cell");
+    var winnerMessage = document.querySelector(".winner-message");
+
+    var nextMove='x';
+
+    button.addEventListener("click", function startNewGame(){
+        for(var i=0; i<cells.length; i++){
+            cells[i].classList.remove("x");
+            cells[i].classList.remove("o");
+        }
+        nextMove='x';
+        winnerMessage.innerHTML = '';
+    });
+    field.addEventListener("click", function handleSendClick(e){
+        //console.log('rgtgrtg');
+        if(!e.target.classList.contains("cell") || e.target.classList.contains("o") || e.target.classList.contains("x")){
+            return;
+        }
+        console.log(getWinner());
+        if(getWinner()){
+            return;
+        }
+        e.target.classList.add(nextMove);
+        if(nextMove==="x")
+            nextMove="o";
+        else
+            nextMove="x";
+        var winner = getWinner();
+        if(winner){
+            if(winner==='x')
+                winnerMessage.innerHTML="Хрестик переміг";
+            else
+                winnerMessage.innerHTML="Нолик переміг";
+        }
+    })
+
+});
